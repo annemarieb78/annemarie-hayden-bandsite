0d9f651c5a56b238370525c33d3a68fa8b724fab
Anne-Marie Hayden <annemariehayden@gmail.com>
2021-12-03 22:40:50 -0500

    adding start of javascript

---
+++ scripts/index-page.js
@@ -0,0 +1,17 @@
+/*const form = document.getElementById("container-comment__fields");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("Form Submitted");
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});*/
+
+document.querySelector("section");
+const sectionElement = document.queryselector("section");
+const showsHeader = document.createElement("h2");
+sectionElement.appendChild("showsHeader");
+
+sectionElement.appendChild(document.createElement("h2"));
