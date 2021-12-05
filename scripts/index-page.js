68cb383859d3c3536a3561b347b21def01a125aa
Anne-Marie Hayden <annemariehayden@gmail.com>
2021-12-05 12:33:42 -0500

    start of comment section .js

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,10 +1,17 @@
-const form = document.getElementById(".container-comment__fields");
+const form = document.querySelector(".container-comment__fields");
+const commentContent = document.querySelector(
+  "container-comment__dynamic-content"
+);
 
 form.addEventListener("click", function (event) {
   event.preventDefault();
+  const nameInput = event.target.nameInput.value;
+  const commentListItem = document.createElement("li");
+  commentListItem.innerText = nameInput;
+  container - comment__dynamic - commentContent.appendChild(commentListItem);
   console.log("Form Submitted");
 });
 
-form.addEventListener("keyup", (event) => {
-  event.preventDefault();
-});
+// form.addEventListener("keyup", (event) => {
+//   event.preventDefault();
+// });
